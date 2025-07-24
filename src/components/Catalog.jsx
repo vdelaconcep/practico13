e800d871e4cb6403c4964fca5d6c8adb45bf32f5
vdelaconcep <vdelaconcep@gmail.com>
2025-07-24 17:10:02 -0300

    Falta adaptar al responsive

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -48,40 +48,45 @@ const Catalog = () => {
         <>
             <div className='catalog-background'>
                 <header className='hstack'>
-                    <h1 className="text-white hal ms-4 mb-0">HAL</h1>
+                    <h1 className="text-white catalog-headerTitle ms-4 mb-0"><span className='catalog-headerTitle-hal'>HAL</span><span className='catalog-headerTitle-movies'>Movies</span></h1>
                     <div className='d-flex ms-auto me-3 me-sm-4 flex-column flex-sm-row align-items-end align-items-sm-center' >
-                        <select
-                            className='catalog-selectGenre mb-2 mb-sm-0 mt-2 mt-sm-0'
-                            name="genres"
-                            id="genres"
-                            value={selectedGenre}
-                            onChange={
-                                (e) => {
-                                    const selectedId = e.target.value;
-                                    setSelectedGenre(selectedId);
+                        <div className='catalog-selectDiv'>
+                            <select
+                                className='catalog-selectGenre mb-2 mb-sm-0 mt-2 mt-sm-0'
+                                name="genres"
+                                id="genres"
+                                value={selectedGenre}
+                                onChange={
+                                    (e) => {
+                                        const selectedId = e.target.value;
+                                        setSelectedGenre(selectedId);
 
-                                    const selected = genresList.find(g => g.id.toString() === selectedId);
-                                    setSelectedGenreName(selected ? selected.name : '');
-                                }}>
-                            <option value="" disabled>{loadingGenres ? 'Loading...' : 'Search by genre'}</option>
-                            {!loadingGenres && genresList && genresList.map(genre => (
-                                <option key={genre.id} value={genre.id}>{genre.name}</option>
-                            ))}
-                        </select>
+                                        const selected = genresList.find(g => g.id.toString() === selectedId);
+                                        setSelectedGenreName(selected ? selected.name : '');
+                                    }}
+                                required>
+                                <option value="" disabled hidden>{loadingGenres ? 'Loading...' : 'Search genre'}</option>
+                                {!loadingGenres && genresList && genresList.map(genre => (
+                                    <option key={genre.id} value={genre.id}>{genre.name}</option>
+                                ))}
+                            </select>
+                            <p className='catalog-selectChevron'><i className="fa-solid fa-chevron-down"></i></p>
+                        </div>
                         <form onSubmit={(e) => {
                             e.preventDefault();
                             setSearchTitle(true);
                         }}>
-                            <div className='ms-sm-4'>
+                            <div className='catalog-searchDiv ms-sm-4'>
                                 <input
                                     className='catalog-searchTitle'
                                     type="search"
                                     placeholder='Search title'
+                                    maxLength={40}
                                     value={title}
                                     onChange={(e) => setTitle(e.target.value)}
                                 />
                                 <button className='catalog-searchTitleButton' type='submit'>
-                                    <i className="fa-solid fa-magnifying-glass catalog-searchIcon"></i>
+                                    <i className="fa-solid fa-magnifying-glass"></i>
                                 </button>
                             </div>
                         </form>
